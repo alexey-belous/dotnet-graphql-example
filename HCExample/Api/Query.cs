cb8c7b047bf1bccd176dbd082fb14318e061d1de
Alexey Belous <alexey.belous@sigma.software>
2021-06-08 13:24:31 +0300

    Initial commit

---
+++ HCExample/Api/Query.cs
@@ -0,0 +1,21 @@
+﻿using HCExample.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCExample.Api
+{
+    public class Query
+    {
+        public Book GetBook() =>
+            new Book
+            {
+                Title = "C# in depth.",
+                Author = new Author
+                {
+                    Name = "Jon Skeet"
+                }
+            };
+    }
+}
---
+++ HCExample/Domain/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCExample.Domain
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+}
